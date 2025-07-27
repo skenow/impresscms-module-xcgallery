d8771d1125e923ee12184ac419fa0b2203049f45
Steve Kenow <skenow@impresscms.org>
2025-07-27 14:28:40 -0500

    Adding in 2 columns that I hadn't added to the repository
    I use the title and album id in the title attribute of photos

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -166,7 +166,7 @@ function get_pic_data($album, &$count, &$album_name, $limit1 = -1, $limit2 = -1,
 	if ($limit2 == 1) {
 		$select_columns = '*';
 	} else {
-		$select_columns = 'pid, filepath, filename, url_prefix, filesize, pwidth, pheight, ctime';
+		$select_columns = 'pid, filepath, filename, url_prefix, filesize, pwidth, pheight, ctime, title, aid';
 	}
 
 	// Regular albums
